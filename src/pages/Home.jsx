d2c68266d26e94e64dfcb7a0759cc6f0140ee5a8
Ayushjgv <ayushprajapat456@gmail.com>
2026-04-07 15:54:28 +0530

    added taks option and search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,6 +21,10 @@ import FormatAlignRightIcon from '@mui/icons-material/FormatAlignRight';
 import LogoutIcon from '@mui/icons-material/Logout';
 import DownloadIcon from '@mui/icons-material/Download';
 import ShareIcon from '@mui/icons-material/Share';
+import AddTaskIcon from '@mui/icons-material/AddTask';
+import DoneIcon from '@mui/icons-material/Done';
+import RemoveDoneIcon from '@mui/icons-material/RemoveDone';
+import { set } from 'firebase/database';
 
 const Home = () => {
   const [showPopup, setShowPopup] = useState(false);
@@ -33,7 +37,8 @@ const Home = () => {
   const [align, setAlign] = useState("left");
   const [isDownload, setisDownload] = useState(false);
   const [downloadType, setDownloadType] = useState("txt");
-
+  const [search, setSearch] = useState("");
+  const [taskInputs, setTaskInputs] = useState([]);
 
   const firebase = useFirebase();
   const navigate = useNavigate();
@@ -57,7 +62,7 @@ const Home = () => {
     });
 
 
-  }, [firebase.isLoggedIn, firebase.User]);
+  }, [firebase.isLoggedIn, firebase.User, currFile]);
 
 
   const handleAddButton = () => {
@@ -88,9 +93,23 @@ const Home = () => {
     firebase.addNote(newFile);
   };
 
-  const handleFileClick = (file) => {
+  const handleFileClick = async (file) => {
     setcurrFile(file);
-    setEditorText(file.note.content || "");
+    if (!file.note.type || file.note.type === "note") {
+      setEditorText(file.note.content || "");
+    } else {
+      let loadedTasks = [];
+      if (file.note.taskinputs && file.note.taskinputs.length > 0) {
+        loadedTasks = file.note.taskinputs.map((item) => {
+          if (typeof item === 'string') return { text: item, done: false };
+          if (typeof item === 'object' && item !== null) return item;
+          return { text: "", done: false };
+        });
+      } else if (file.note.content) {
+        loadedTasks = file.note.content.split("\n").filter(t => t.trim() !== "").map(text => ({ text, done: false }));
+      }
+      setTaskInputs(loadedTasks);
+    }
   };
 
   const handleLogout = () => {
@@ -109,7 +128,7 @@ const Home = () => {
 
   const handleEditor = (e) => {
     setEditorText(e.target.value);
-    firebase.updateNote(currFile.id, { note: { name: currFile.note.name, content: e.target.value } });
+    firebase.updateNote(currFile.id, { note: { name: currFile.note.name, content: e.target.value, type: currFile.note.type } });
   }
 
   const handleDownload = () => {
@@ -137,6 +156,75 @@ const Home = () => {
       document.body.removeChild(element);
     }
   }
+
+  const handleOpenFile = () => {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = ".txt,.md,.pdf";
+
+    document.body.appendChild(input);
+
+    input.onchange = (e) => {
+      const file = e.target.files[0];
+      if (!file) return;
+
+      if (file.type === "application/pdf") {
+        alert("PDF not supported yet");
+        document.body.removeChild(input);
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const content = e.target.result;
+        const newFile = {
+          id: Date.now(),
+          name: file.name,
+          type: "note",
+          content: content
+        };
+
+        setallFiles((prev) => [newFile, ...prev]);
+        setcurrFile(newFile);
+        setEditorText(content);
+        firebase.addNote(newFile);
+        document.body.removeChild(input);
+        window.location.reload();
+
+      };
+      reader.readAsText(file);
+    };
+
+    input.click();
+
+  };
+
+  const syncTasks = () => {
+    if (currFile.note.type === "task") {
+      firebase.updateNote(currFile.id, { note: { name: currFile.note.name, content: taskInputs.map(t => t.text).join("\n"), type: "task", taskinputs: taskInputs } });
+    }
+  }
+
+  const handleDone = (index) => {
+    setTaskInputs((prev) => {
+      const newInputs = [...prev];
+      newInputs[index] = { ...newInputs[index], done: !newInputs[index].done };
+      return newInputs;
+    });
+  };
+
+  const handleDeleteTask = (index) => {
+    setTaskInputs((prev) => {
+      const newInputs = [...prev];
+      newInputs.splice(index, 1);
+      return newInputs;
+    });
+  };
+
+
+
+
+
 
 
   return (
@@ -232,6 +320,8 @@ const Home = () => {
               type="text"
               className='w-full bg-transparent outline-none placeholder-gray-400 text-sm text-gray-800 ml-2 focus:outline-none focus:ring-0'
               placeholder='Search notes and tags...'
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
             />
           </div>
         </div>
@@ -244,19 +334,24 @@ const Home = () => {
               if (currFile == null) {
                 handleFileClick(file);
               }
-              return (
-                <div onClick={() => handleFileClick(file)} key={file.id} className="list-items flex items-center justify-between p-3 border border-gray-200 rounded-xl cursor-pointer hover:bg-gray-50 transition-colors">
-                  <span className="text-sm font-medium text-gray-700">{file.note.name}</span>
-                  <div>
-                    <button onClick={() => handleFileClick(file)} className="text-gray-400 hover:text-gray-900 hover:bg-gray-100 rounded-md transition-colors cursor-pointer">
-                      <SyncIcon fontSize='small' />
-                    </button>
-                    <button onClick={() => handleDelete(file.id)} className="text-gray-400 hover:text-gray-900 hover:bg-gray-100 rounded-md transition-colors cursor-pointer">
-                      <DeleteIcon fontSize='small' />
-                    </button>
+              if (search.trim() !== "" && !file.note.name.toLowerCase().includes(search.toLowerCase())) {
+                return null;
+              } else {
+                return (
+                  <div onClick={() => handleFileClick(file)} key={file.id} className="list-items flex items-center justify-between p-3 border border-gray-200 rounded-xl cursor-pointer hover:bg-gray-50 transition-colors">
+                    {file.note.type == "task" ? <NoteAltIcon fontSize='small' /> : <EditIcon fontSize='small' />}
+                    <span className="text-sm font-medium text-gray-700">{file.note.name}</span>
+                    <div>
+                      <button onClick={() => handleFileClick(file)} className="text-gray-400 hover:text-gray-900 hover:bg-gray-100 rounded-md transition-colors cursor-pointer">
+                        <SyncIcon fontSize='small' />
+                      </button>
+                      <button onClick={() => handleDelete(file.id)} className="text-gray-400 hover:text-gray-900 hover:bg-gray-100 rounded-md transition-colors cursor-pointer">
+                        <DeleteIcon fontSize='small' />
+                      </button>
+                    </div>
                   </div>
-                </div>
-              )
+                )
+              }
             }) : <div className="flex items-center justify-center h-full">
               <span className="text-sm font-medium text-gray-700">No files Yet</span>
             </div>
@@ -291,7 +386,7 @@ const Home = () => {
             <button className='px-3 py-1.5 text-sm font-medium text-gray-500 hover:text-gray-900 rounded-md transition-colors duration-150 cursor-pointer focus:outline-none'>
               <ShareIcon fontSize='small' />
             </button>
-            <button className='px-4 py-1.5 text-sm font-medium text-gray-700 hover:text-black rounded-md transition-colors duration-150 cursor-pointer  focus:outline-none ml-2'>
+            <button onClick={() => { handleOpenFile() }} className='px-4 py-1.5 text-sm font-medium text-gray-700 hover:text-black rounded-md transition-colors duration-150 cursor-pointer  focus:outline-none ml-2'>
               <FileOpenIcon fontSize='small' />
             </button>
             <button onClick={() => { setisDownload(true) }} className='px-4 py-1.5 text-sm font-medium text-gray-700 hover:text-black rounded-md transition-colors duration-150 cursor-pointer focus:outline-none ml-2'>
@@ -305,7 +400,7 @@ const Home = () => {
 
         {/* //editor */}
         {
-          currFile ? (
+          currFile && (currFile.note.type == "note" || !currFile.note.type) ? (
             <div className='flex-1 flex flex-col px-8 md:px-16 pt-4 pb-12 overflow-y-auto'>
               <div className='mx-auto w-full flex-1 flex flex-col'>
                 <textarea
@@ -318,7 +413,48 @@ const Home = () => {
                   placeholder="Press Enter to continue typing..."
                 ></textarea>
               </div>
-            </div>) : (<div className='flex-1 px-8 md:px-16 pt-4 pb-20 overflow-y-auto flex items-center justify-center'>
+            </div>) : currFile && currFile.note.type == "task" ? (
+              <div className='flex-1 flex flex-col px-8 md:px-16 pt-4 pb-12 overflow-y-auto'>
+                {/* topbar */}
+                <div className='flex items-center justify-between mb-4'>
+                  <button onClick={() => { setTaskInputs([...taskInputs, { text: "New Task", done: false }]) }} className='flex bg-blue-300 rounded-sm'><AddTaskIcon fontSize='small' /></button>
+                  <button onClick={syncTasks}><SyncIcon fontSize='small' /></button>
+                </div>
+                {/* list */}
+                {
+                  taskInputs.length > 0 ?
+                    taskInputs.map((task, index) => {
+                      return (
+                        <div key={index} className='tasks flex p-3 border items-center border-gray-200 rounded-xl hover:bg-gray-50 transition-colors'>
+                          <button onClick={() => handleDone(index)} className="mr-2"><DoneIcon fontSize='small' /></button>
+                          <input
+                            type="text"
+                            value={task.text || ""}
+                            onChange={(e) => {
+                              const newInputs = [...taskInputs];
+                              newInputs[index] = { ...newInputs[index], text: e.target.value };
+                              setTaskInputs(newInputs);
+                            }}
+                            style={{ textDecoration: task.done ? 'line-through' : 'none' }}
+                            className={`w-full bg-transparent outline-none ${task.done ? 'text-gray-400' : 'text-gray-800'}`}
+                          />
+                          <button onClick={() => handleDeleteTask(index)} className="ml-2 text-gray-400 hover:text-red-500 cursor-pointer"><DeleteIcon fontSize='small' /></button>
+                        </div>
+                      );
+                    })
+                    :
+                    (
+
+                      <div className="flex items-center justify-center h-full">
+                        No Tasks Yet
+                      </div>
+                    )
+                }
+
+              </div>
+            )
+
+            : (<div className='flex-1 px-8 md:px-16 pt-4 pb-20 overflow-y-auto flex items-center justify-center'>
               <div className='mx-auto text-center'>
                 <span className='text-sm font-medium text-gray-400'>No file selected</span>
               </div>
